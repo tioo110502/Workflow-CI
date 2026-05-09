85f3c24d091790af62e3986073d87e38d8772983
tioo110502 <109257265+tioo110502@users.noreply.github.com>
2026-05-09 23:58:58 +0700

    Refactor model training to use a pipeline

--- build_model.py
+++ build_model.py
@@ -1,6 +1,7 @@
 import os
 import mlflow.sklearn
 import pandas as pd
+from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
@@ -11,11 +12,12 @@ y = df[df.columns[-1]]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-vectorizer = TfidfVectorizer()
-X_train_vec = vectorizer.fit_transform(X_train)
+model = Pipeline([
+    ("tfidf", TfidfVectorizer()),
+    ("model", LogisticRegression(max_iter=1000))
+])
 
-model = LogisticRegression(max_iter=1000)
-model.fit(X_train_vec, y_train)
+model.fit(X_train, y_train)
 
 mlflow.sklearn.save_model(model, "./local_model")
 print("Model saved to ./local_model")
